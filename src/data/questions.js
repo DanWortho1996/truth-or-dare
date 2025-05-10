524998fa02b1a41310deaedba0ee92ad7878090f
Daniel Worthington <daniel.worthington96@outlook.com>
2025-05-10 17:27:30 +0100

    add Q's for truth & dare

--- src/data/questions.js
+++ src/data/questions.js
@@ -3,13 +3,23 @@ export const truthQuestions = [
   "Have you ever lied to a friend?",
   "What's something embarrassing you've done?",
   "Who was your first crush?",
-  "What’s a secret you’ve never told anyone?"
+  "What’s a secret you’ve never told anyone?",
+  "Who is your favourite Musician from the 80's?",
+  "What Football team do you support?",
+  "Do you think you can Sing?",
+  "Have you ever told a lie what turned out to be true?",
+  "Have you been on television before?"
 ];
 
 export const dareQuestions = [
-  "Do 10 jumping jacks!",
+  "Do 10 star jumps!",
   "Sing a song loudly!",
   "Dance without music for 30 seconds.",
   "Speak in a funny accent for a minute.",
-  "Act like a chicken for 10 seconds."
+  "Act like a chicken for 10 seconds.",
+  "Prank call the last person in your recent call log",
+  "Run to the stop of stairs within 10 seconds!",
+  "Pour a drink on your own head",
+  "Scream out a window 'FANCY A PINT'",
+  "Play your most hated song"
 ];
